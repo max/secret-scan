872c0fdaefd12020f2324c42be4512438f1ff64e
Ji <jjeonie@gmail.com>
2020-08-25 21:55:30 +1200

    Update logic to execute TruffleHog

--- README.md
+++ README.md
@@ -14,3 +14,21 @@ jobs:
       - uses: actions/checkout@v2
       - uses: max/secret-scan@master
 ```
+
+## Example with path filters
+
+```yaml
+on: push
+name: Find Secrets
+jobs:
+  main:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: max/secret-scan@master
+        with:
+          include_path: 'configuration/include_paths.txt'
+          exclude_path: 'configuration/exclude_paths.txt'
+```
+
+For more information about the format of the configuration files for include / exclude paths, please refer to [TruffleHog](https://github.com/dxa4481/truffleHog).
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,16 @@
 
 set -e
 
-echo "Welcome to the action: Secret Scan! TruffleHog will run shortly. More information about TruffleHog is available at: https://github.com/dxa4481/truffleHog"
-
-trufflehog --include_paths $INPUT_EXCLUDE_PATH --exclude_paths $INPUT_EXCLUDE_PATH $GITHUB_WORKSPACE
+if [ -f "$INPUT_INCLUDE_PATH" ] && [[ -f "$INPUT_EXCLUDE_PATH" ]]; then
+    echo "Running TruffleHog with include and exclude paths."
+    trufflehog --include_paths $INPUT_INCLUDE_PATH --exclude_paths $INPUT_EXCLUDE_PATH $GITHUB_WORKSPACE
+elif [ -f "$INPUT_INCLUDE_PATH" ] ; then
+    echo "Running TruffleHog with include paths."
+    trufflehog --include_paths $INPUT_INCLUDE_PATH $GITHUB_WORKSPACE
+elif [ -f "$INPUT_EXCLUDE_PATH" ] ; then
+    echo "Running TruffleHog with exclude paths."
+    trufflehog --exclude_paths $INPUT_EXCLUDE_PATH $GITHUB_WORKSPACE
+else
+    echo "Running TruffleHog without path filters."
+    trufflehog $GITHUB_WORKSPACE
+fi
