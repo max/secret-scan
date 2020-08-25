e1276129d09bd7eb45e72035ac8a00acfc47d56a
Ji <jjeonie@gmail.com>
2020-08-25 19:44:23 +1200

    Update paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ echo "GitHub Workspace $GITHUB_WORKSPACE"
 echo "Include Path $INPUT_EXCLUDE_PATH"
 echo "Exclude Path $INPUT_INCLUDE_PATH"
 
-trufflehog --include_paths 'Configuration/include-patterns.txt' --exclude_paths 'Configuration/exclude-patterns.txt' $GITHUB_WORKSPACE
+trufflehog --include_paths 'Configuration/include_paths.txt' --exclude_paths 'Configuration/exclude_paths.txt' $GITHUB_WORKSPACE
