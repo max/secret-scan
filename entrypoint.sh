40050d570cd4601f2e60d30bb150bd7c51de1005
Ji <jjeonie@gmail.com>
2020-08-25 19:49:44 +1200

    Remove parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 echo "Include Path $INPUT_EXCLUDE_PATH"
 echo "Exclude Path $INPUT_INCLUDE_PATH"
 
-trufflehog --include_paths 'Configuration/include_paths.txt' --exclude_paths 'Configuration/exclude_paths.txt' $GITHUB_WORKSPACE
+trufflehog $GITHUB_WORKSPACE
