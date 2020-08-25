ab100bfd20f2ac9c7eba398227d2fedef535496b
Ji <jjeonie@gmail.com>
2020-08-25 19:32:52 +1200

    Add step to checkout code

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,15 @@
 
 set -e
 echo "GitHub Workspace $GITHUB_WORKSPACE"
-echo "GitHub Workspace $GITHUB_WORKSPACE"
-echo "GitHub Workspace $GITHUB_WORKSPACE"
+echo "Include Path $INPUT_EXCLUDE_PATH"
+echo "Exclude Path $INPUT_INCLUDE_PATH"
+
 printenv
+
+ls
+
+cd $GITHUB_WORKSPACE
+
+ls
 
 trufflehog $GITHUB_WORKSPACE
