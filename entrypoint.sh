ccd86df696409f000d6224fb9dd29922bcc79bf6
José Canal <jose.canal@betalearning.com.br>
2021-10-19 14:24:19 -0300

    Refactor entrypoint and add --allow argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,21 @@
 
 set -e
 
-if [ -f "$INPUT_INCLUDE_PATH" ] && [[ -f "$INPUT_EXCLUDE_PATH" ]]; then
-    echo "Running TruffleHog with include and exclude paths."
-    trufflehog --include_paths $INPUT_INCLUDE_PATH --exclude_paths $INPUT_EXCLUDE_PATH $GITHUB_WORKSPACE
-elif [ -f "$INPUT_INCLUDE_PATH" ] ; then
-    echo "Running TruffleHog with include paths."
-    trufflehog --include_paths $INPUT_INCLUDE_PATH $GITHUB_WORKSPACE
-elif [ -f "$INPUT_EXCLUDE_PATH" ] ; then
-    echo "Running TruffleHog with exclude paths."
-    trufflehog --exclude_paths $INPUT_EXCLUDE_PATH $GITHUB_WORKSPACE
-else
-    echo "Running TruffleHog without path filters."
-    trufflehog $GITHUB_WORKSPACE
-fi
+# https://github.com/koalaman/shellcheck/wiki/Sc2086#exceptions
+# https://wiki.bash-hackers.org/syntax/pe#use_an_alternate_value
+# https://unix.stackexchange.com/questions/131766/why-does-my-shell-script-choke-on-whitespace-or-other-special-characters/131767#131767
+# Prepare the command
+set -- trufflehog
+set -- "$@" ${INPUT_INCLUDE_PATH:+--include_paths "$INPUT_INCLUDE_PATH"}
+set -- "$@" ${INPUT_EXCLUDE_PATH:+--exclude_paths "$INPUT_EXCLUDE_PATH"}
+set -- "$@" ${INPUT_ALLOWED_PATTERNS:+--allow "$INPUT_ALLOWED_PATTERNS"}
+set -- "$@" "$GITHUB_WORKSPACE"
+
+# Print the command
+cat << COMMAND > /dev/stdout
+Running truffleHog with command:
+$@
+COMMAND
+
+# Run the command
+"$@"
